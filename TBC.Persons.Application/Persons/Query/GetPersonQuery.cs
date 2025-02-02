13fc6a335d5b4be11791c24b8b459bca1adcd9a6
langu <elkanishvili.guram@gmail.com>
2025-02-03 02:18:53 +0400

    included migrations

---
+++ TBC.Persons.Application/Persons/Query/GetPersonQuery.cs
@@ -0,0 +1,14 @@
+﻿using MediatR;
+using TBC.Persons.Domain.Interfaces;
+
+namespace TBC.Persons.Application.Persons.Query;
+
+public record GetPersonQuery(long Id) : IRequest<Person?>;
+
+public class GetPersonQueryHandler(IPersonsRepository db) : IRequestHandler<GetPersonQuery, Person?>
+{
+    public async Task<Person?> Handle(GetPersonQuery request, CancellationToken cancellationToken)
+    {
+        return await db.GetByIdAsync(request.Id, cancellationToken: cancellationToken);
+    }
+}
