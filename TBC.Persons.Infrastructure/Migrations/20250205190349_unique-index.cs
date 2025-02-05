c1ee96b9603a19676ec62445cddfd9acb4c269ca
langu <elkanishvili.guram@gmail.com>
2025-02-05 23:05:11 +0400

    included migrations

---
+++ TBC.Persons.Infrastructure/Migrations/20250205190349_unique-index.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TBC.Persons.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class uniqueindex : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_PhoneNumbers_Type_Number",
+                table: "PhoneNumbers",
+                columns: new[] { "Type", "Number" },
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_PhoneNumbers_Type_Number",
+                table: "PhoneNumbers");
+        }
+    }
+}
